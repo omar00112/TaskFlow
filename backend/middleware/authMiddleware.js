f9de8a1e97b6546e7fc225865e19eb199a0a8a2d
omar00112 <omaramgharpc123@gmail.com>
2026-05-06 02:29:28 +0100

    feat: add authentication middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,13 @@
+const jwt = require('jsonwebtoken');
+
+// middleware pour protéger les routes authentifiées
+module.exports = (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '');
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        res.status(401).json({ message: "Accès refusé. Token invalide." });
+    }
+};
